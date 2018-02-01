3d79b7d08c5ff3f78cb2fca25fcc333d11275b39
Andrew <a.massamillo@gmail.com>
2018-02-01 09:49:40 -0500

    get todos

--- api.js
+++ api.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 const port = 4000
-app.get('/', (req, res) => res.send('<h1>Welcome to the ToDos API</h1>'))
 
-app.listen(port, () => console.log('TODOS API IS UP!', port))
+console.log('process.env', process.env)
+
+const todos = [
+  { id: 1, text: "Wake up", completed: true },
+  { id: 2, text: "Drink coffee", completed: true },
+  { id: 3, text: "Teach express", completed: false }
+]
+
+
+app.get('/', (req, res) => res.send('<h1>Welcome to the ToDos API</h1>'))
+app.get('/todos', (req, res) => res.send(todos))
+
+app.listen(port, () => console.log('TODOS API IS UP! on port', port))
